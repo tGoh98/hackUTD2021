88477056ce812b3a7383960b188767bb97febaaa
tGoh98 <tmg5@rice.edu>
2021-02-27 12:37:11 -0600

    Initial Commit

---
+++ hackUTD21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hackUTD21
+//
+//  Created by Timothy Goh on 2/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hackUTD21/hackUTD21App.swift
@@ -0,0 +1,17 @@
+//
+//  hackUTD21App.swift
+//  hackUTD21
+//
+//  Created by Timothy Goh on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct hackUTD21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
